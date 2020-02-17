fb09a4d001d5d1496dd262ffa576698fab355f72
Bobby Hall Jr <bobbyhalljr@Bobbys-MacBook.local>
2020-02-17 12:01:29 -0500

    trying to fix mongoose connection error

--- graphql/resolvers/coins.js
+++ graphql/resolvers/coins.js
@@ -4,11 +4,14 @@ module.exports = {
     Query: {
         async coins(_, { id, name, symbol, price, likes, comments, chats }){
             await coin.find()
+        },
+        async users(_, { username, token }){
+            await this.users.find()
         }
     },
     Mutations: {
         async createComment(_, { id, body }, context){
-            
+
         }
     }
 }
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const typeDefs = require('./graphql/typdefs');
 const resolvers = require('./graphql/resolvers')
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 
 dotenv.config()
 
@@ -10,13 +12,36 @@ dotenv.config()
 const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: ({ req }) => ({ req,  })
+    context: ({ req }) => ({ req })
 })
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true,})
-.then(() => {
-    return server.listen({ port: process.env.PORT || 4000 }).then(res => {
-      console.log(`Server running at ${res.url}`);
-      console.log('mongodb database connected!')
-    });
+// Connection URL
+const url = 'mongodb+srv://bobby:Milo0215@crypto-tracker-backend-ofdi0.mongodb.net/test?retryWrites=true&w=majority';
+
+// Database Name
+const dbName = 'crypto-tracker';
+
+// Create a new MongoClient
+const client = new MongoClient(url, { useUnifiedTopology: true });
+
+// Use connect method to connect to the Server
+client.connect(function(err) {
+  // assert.equal(null, err);
+  console.log("Connected successfully to mongodb DB");
+
+  const db = client.db(dbName);
+
+  client.close();
 });
+
+
+
+// .then(() => {
+    server.listen({ port: process.env.PORT || 4000 })
+    .then(res => {
+      console.log(`Server running at ${res.url}`)
+      // console.log('mongodb database connected!')
+    })
+    .catch(error => console.log(error))
+// })
+// .catch(error => console.log(error))
